452ba2e428c4a987ea3d9e677f9637bbb65168f7
sk7527228-commits <sk7527228@gmail.com>
2026-05-21 12:01:34 +0500

    Add files via upload

---
+++ file no 1.py
@@ -0,0 +1,31 @@
+a = ("musakhan")
+print(a)
+ali = 'this is king'
+print (ali.upper())
+ali = 'THIS IS KING'
+print(ali.lower())
+ali = 'this is king '
+musa = ali.split()
+print(len(musa))
+ali = 'this is    \bking'
+print(ali)
+ali = 'this is\tking'
+print(ali)
+ali = 'this is\rking'
+print(ali)
+ali = 'this is\nking'
+print(ali)
+ali = 'this is king'
+print(ali[1:10])
+txt = "this is king"
+print(txt.capitalize())
+txt = "this is king"
+print(txt.center(100))
+txt = "this is king of deomen joke joke joke joke"
+print(txt.count("joke"))
+txt = "my name is khan"
+print(txt.encode())
+txt = "my name is khan khan"
+print(txt.endswith('khan'))
+txt = "my name is khan \tkhan"
+print(txt.expandtabs(4))
