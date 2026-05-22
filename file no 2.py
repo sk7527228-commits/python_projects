5427a45eb1448e5de911883ca4275bb3c0c685fe
sk7527228-commits <sk7527228@gmail.com>
2026-05-22 11:44:50 +0500

    Add files via upload

---
+++ file no 2.py
@@ -0,0 +1,45 @@
+a = 4
+b = 6
+c = a +b
+print (c)
+a = 5
+b = 6
+c = a - b
+print(c)
+a = 5
+b = 6
+c = a * b
+print(c)
+a = 5
+b = 6
+c = a / b
+print(c)
+a = 5
+b = 6
+c = a % b
+print(c)
+a = 5
+b = 9
+c = a // b
+print(c)
+a = 5
+b = 6
+c = a ** b
+print(c)
+a = 5
+b = 6
+c = a + b
+print(c)
+x = 5
+x&= 5
+print(x)
+print(5 < 7) and (7>5)
+print(5 < 7) or (7 > 5)
+print(5 > 7) is (7 < 5)
+a = 'ali'
+b = 'ali'
+print(a and b)
+a = 'this is musa khan '
+print ('khan' in a)
+a = 'this is musa khan '
+print ('Khan' in a)
