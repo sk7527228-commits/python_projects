7795a626f2335a9034d33c65aeb71280a1bc2ff1
sk7527228-commits <sk7527228@gmail.com>
2026-05-23 10:39:39 +0500

    Add files via upload

--- file no 2.py
+++ file no 2.py
@@ -43,3 +43,30 @@ a = 'this is musa khan '
 print ('khan' in a)
 a = 'this is musa khan '
 print ('Khan' in a)
+
+
+print("    STUDENT INFORMATION  ")
+print("=" * 50)
+print("          STUDENT CARD ")
+print("=" * 50)
+
+student_name = input("ENTER STUDENT NAME : ")
+father_name = input("ENTER FATHER NAME  : ")
+school_name = input("ENTER SCHOOL NAME  : ")
+student_class = input("ENTER CLASS        : ")
+address = input("ENTER ADDRESS      : ")
+
+print( "=" * 50)
+print("             STUDENT INFORMATION ")
+print("=" * 50)
+
+print(f" STUDENT NAME : {student_name}")
+print(f" FATHER NAME  : {father_name}")
+print(f" SCHOOL NAME  : {school_name}")
+print(f" CLASS        : {student_class}")
+print(f" ADDRESS      : {address}")
+
+print("=" * 50)
+print("        MADE BY MUSA AJMAL")
+print("=" * 50)
+
