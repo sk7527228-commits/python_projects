b2f1ede64c8e13f795029947d15326e0b5b2ce89
sk7527228-commits <sk7527228@gmail.com>
2026-06-03 11:54:08 +0500

    Add files via upload

---
+++ .github/workflows/file no 1.py
@@ -0,0 +1,43 @@
+a = ("musakhan")
+print(a)
+ali = 'this is king'
+print (ali.upper())
+ali = 'THIS IS KING'
+print(ali.lower())
+ali = 'this is king '
+musa = ali.split()
+print(len(musa))
+ali = 'this is    \bking'
+print(ali)
+ali = 'this is\tking'
+print(ali)
+ali = 'this is\rking'
+print(ali)
+ali = 'this is\nking'
+print(ali)
+ali = 'this is king'
+print(ali[1:10])
+txt = "this is king"
+print(txt.capitalize())
+txt = "this is king"
+print(txt.center(100))
+txt = "this is king of deomen joke joke joke joke"
+print(txt.count("joke"))
+txt = "my name is khan"
+print(txt.encode())
+txt = "my name is khan khan"
+print(txt.endswith('khan'))
+txt = "my name is khan \tkhan"
+txt = "my name is khan khan"
+txt = 'my name is khan'
+print(txt.replace('khan', 'joke'))
+txt = 'my name is khan khan'
+print(txt.find("khan"))
+txt = 'my name is khan khan'
+print(txt.index('khan'))
+print(10>5)
+print (bool('king'))
+print (bool(15))
+print (bool())
+text = "there is somthing worng at the bottom of the screen please check it quickly"
+print(text)
